30601b8230f0e356cac1f82cfa70c18a16e86517
qummetov.m <you@example.comqummetov.m@gmail.com>
2019-10-14 09:08:56 +0400

    Add fetching list

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+export class App extends React.Component {
+  state = {
+    isLoading: true,
+    users: [],
+    error: null
+  };
+
+  fetchUsers() {
+    fetch(`https://jsonplaceholder.typicode.com/todos`)
+        .then(response => response.json())
+        .then(data =>
+            this.setState({
+              users: data,
+              isLoading: false,
+            })
+        )
+        .catch(error => this.setState({ error, isLoading: false }));
+  }
+
+  componentDidMount() {
+    this.fetchUsers();
+  }
+  render() {
+    const { isLoading, users, error } = this.state;
+    return (
+        <React.Fragment>
+          {error ? <p>{error.message}</p> : null}
+          {!isLoading ? (
+              users.map(user => {
+                const { id, title } = user;
+                return (
+                    <div key={id}>
+                      <span>{id} </span>
+                      <span> {title}</span>
+                    </div>
+                );
+              })
+          ) : (
+              <h3>Loading...</h3>
+          )}
+        </React.Fragment>
+    );
+  }
+}
 export default App;
