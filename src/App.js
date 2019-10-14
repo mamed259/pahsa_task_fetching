c471ca6e2546b01adcaf85e9c84854d0f1fdaa76
qummetov.m <you@example.comqummetov.m@gmail.com>
2019-10-14 11:41:50 +0400

    Add some changes

--- src/App.js
+++ src/App.js
@@ -6,43 +6,84 @@ export class App extends React.Component {
   state = {
     isLoading: true,
     users: [],
-    error: null
+    todos: [],
+    selectValue : 0
   };
 
-  fetchUsers() {
-    fetch(`https://jsonplaceholder.typicode.com/todos`)
-        .then(response => response.json())
-        .then(data =>
-            this.setState({
-              users: data,
-              isLoading: false,
-            })
-        )
-        .catch(error => this.setState({ error, isLoading: false }));
-  }
 
   componentDidMount() {
-    this.fetchUsers();
+    this.setState({
+      isLoading: true
+    });
+
+    Promise.all([
+      fetch("https://jsonplaceholder.typicode.com/todos"),
+      fetch("https://jsonplaceholder.typicode.com/users")
+    ])
+        .then(([resp1, resp2]) => {
+          return Promise.all([resp1.json(), resp2.json()])
+        })
+        .then(([resp1, resp2]) => {
+          this.setState(
+              {
+                todos: resp1,
+                users: resp2,
+                isLoading: true
+              }
+          )
+        })
   }
+
+    getTodo = id => {
+        this.setState({
+            isLoading: false
+        });
+        fetch("https://jsonplaceholder.typicode.com/todos?userId=" + id)
+            .then(response => response.json())
+            .then(data => {
+                this.setState({
+                    todos: data,
+                    isLoading: true
+                })
+            })
+    };
+
+  change = event => {
+    this.setState({value: event.target.selectValue});
+    this.getTodo(event.target.value)
+  };
+
+
   render() {
-    const { isLoading, users, error } = this.state;
     return (
-        <React.Fragment>
-          {error ? <p>{error.message}</p> : null}
-          {!isLoading ? (
-              users.map(user => {
-                const { id, title } = user;
-                return (
-                    <div key={id}>
-                      <span>{id} </span>
-                      <span> {title}</span>
-                    </div>
-                );
-              })
-          ) : (
-              <h3>Loading...</h3>
-          )}
-        </React.Fragment>
+        <div className="content">
+          <div className="select-box">
+            <select onChange={this.change} value={this.state.selectValue}>
+              <option>All</option>
+              {this.state.users.map(user => {
+                return(
+                    <option key={user.id} value={user.id}>{user.name}</option>
+                )
+              })}
+            </select>
+          </div>
+          { this.state.isLoading
+              ? <div>
+                {
+                  this.state.todos.map(user => {
+                      const { id, title } = user;
+                      return (
+                            <div key={id}>
+                                <span>{id} </span>
+                                <span>{title}</span>
+                            </div>
+                      );
+                  })
+                }
+              </div>
+              : <div>Loading...</div>
+          }
+        </div>
     );
   }
 }
